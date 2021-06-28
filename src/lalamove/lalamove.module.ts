167b80c857c4897364e0e431c13dbdcc718a65bb
Muhaimin <muhaiminmfadzil@gmail.com>
2021-06-28 10:30:31 +0800

    dynamic module for root config

--- src/lalamove/lalamove.module.ts
+++ src/lalamove/lalamove.module.ts
@@ -1,7 +1,22 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Global, HttpModule, Module } from '@nestjs/common';
+import { CONFIG_OPTIONS, LalamoveOptions } from './lalamove.definition';
 import { LalamoveService } from './lalamove.service';
 
-@Module({
-  providers: [LalamoveService]
-})
-export class LalamoveModule {}
+@Global()
+@Module({})
+export class LalamoveModule {
+  static forRoot(options: LalamoveOptions): DynamicModule {
+    return {
+      module: LalamoveModule,
+      imports: [HttpModule],
+      providers: [
+        {
+          provide: CONFIG_OPTIONS,
+          useValue: options,
+        },
+        LalamoveService,
+      ],
+      exports: [LalamoveService],
+    };
+  }
+}
